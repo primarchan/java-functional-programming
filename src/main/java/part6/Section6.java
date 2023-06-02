333c65d218b0579c7fa4a4697d11bae86c37ff27
An T.G <boltcarrier@naver.com>
2023-06-03 01:32:20 +0900

    Part6.6 - FlatMap

---
+++ src/main/java/part6/Section6.java
@@ -0,0 +1,63 @@
+package part6;
+
+import part6.model.Order;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Section6 {
+
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(3, -5, 4, -5, 2, 3);
+        List<Integer> distinctNumbers = numbers.stream()
+                .distinct()
+                .collect(Collectors.toList());
+        System.out.println("distinctNumbers : " + distinctNumbers);
+
+        LocalDateTime now = LocalDateTime.now(ZoneId.of("Asia/Seoul"));
+
+        Order order1 = new Order()
+                .setId(1001)
+                .setStatus(Order.OrderStatus.CREATED)
+                .setCreatedByUserId(101)
+                .setCreatedAt(now.minusHours(4));
+
+        Order order2 = new Order()
+                .setId(1002)
+                .setStatus(Order.OrderStatus.ERROR)
+                .setCreatedByUserId(103)
+                .setCreatedAt(now.minusHours(1));
+
+        Order order3 = new Order()
+                .setId(1003)
+                .setStatus(Order.OrderStatus.PROCESSED)
+                .setCreatedByUserId(102)
+                .setCreatedAt(now.minusHours(36));
+
+        Order order4 = new Order()
+                .setId(1004)
+                .setStatus(Order.OrderStatus.ERROR)
+                .setCreatedByUserId(104)
+                .setCreatedAt(now.minusHours(40));
+
+        Order order5 = new Order()
+                .setId(1005)
+                .setStatus(Order.OrderStatus.IN_PROGRESS)
+                .setCreatedByUserId(101)
+                .setCreatedAt(now.minusHours(10));
+
+        List<Order> orders = Arrays.asList(order1, order2, order3, order4, order5);
+
+        List<Long> distinctCreatedByUserIds = orders.stream()
+                .map(Order::getCreatedByUserId)
+                .distinct()
+                .sorted()
+                .collect(Collectors.toList());
+        System.out.println("distinctCreatedByUserIds : " + distinctCreatedByUserIds);
+
+    }
+
+}
