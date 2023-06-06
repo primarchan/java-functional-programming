8ef568e63b005c56e7ebb5aaa975a1746d5b586a
An T.G <boltcarrier@naver.com>
2023-06-06 21:51:07 +0900

    Part8.5 - Collectors

---
+++ src/main/java/part8/Section5.java
@@ -0,0 +1,32 @@
+package part8;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Section5 {
+
+    public static void main(String[] args) {
+        List<Integer> numberList = Stream.of(3, 5, -3, 3, 4, 5)
+                .collect(Collectors.toList());
+        System.out.println("numberList : " + numberList);
+
+        Set<Integer> numberSet = Stream.of(3, 5, -3, 3, 4, 5)
+                .collect(Collectors.toSet());
+        System.out.println("numberSet : " + numberSet);
+
+        List<Integer> numberList2 = Stream.of(3, 5, -3, 3, 4, 5)
+                .collect(Collectors.mapping(x -> Math.abs(x), Collectors.toList()));
+        System.out.println("numberList2 : " + numberList2);
+
+        Set<Integer> numberSet2 = Stream.of(3, 5, -3, 3, 4, 5)
+                .collect(Collectors.mapping(x -> Math.abs(x), Collectors.toSet()));
+        System.out.println("numberSet2 : " + numberSet2);
+
+        int sum = Stream.of(3, 5, -3, 3, 4, 5)
+                .collect(Collectors.reducing(0, (x, y) -> x + y));
+        System.out.println("sum : " + sum);
+    }
+
+}
