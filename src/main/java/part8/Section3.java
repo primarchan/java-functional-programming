9c936ae18c57ef7821a118ee3695a7a17c59763c
An T.G <boltcarrier@naver.com>
2023-06-06 20:58:46 +0900

    Part8.3 - Find First Find Any

---
+++ src/main/java/part8/Section3.java
@@ -0,0 +1,20 @@
+package part8;
+
+import java.util.Optional;
+import java.util.stream.Stream;
+
+public class Section3 {
+
+    public static void main(String[] args) {
+        Optional<Integer> anyNegativeInteger = Stream.of(3, 2, -5, 6)
+                .filter(x -> x < 0)
+                .findAny();
+        System.out.println("anyNegativeInteger : " + anyNegativeInteger);
+
+        Optional<Integer> firstPositiveInteger = Stream.of(3, 2, -5, 6)
+                .filter(x -> x > 0)
+                .findFirst();
+        System.out.println("firstPositiveInteger" + firstPositiveInteger);
+    }
+
+}
