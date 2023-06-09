09e5fbef0a83e16a90d66d609742eb5c2faf76a5
An T.G <boltcarrier@naver.com>
2023-06-10 02:58:13 +0900

    Part9.1 - Scope Closure Curry

---
+++ src/main/java/part9/Section1.java
@@ -0,0 +1,32 @@
+package part9;
+
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+public class Section1 {
+
+    public static void main(String[] args) {
+        Supplier<String> supplier = getStringSupplier();
+        System.out.println(supplier.get());
+
+        BiFunction<Integer, Integer, Integer> add = (x, y) -> x + y;
+        Function<Integer, Function<Integer, Integer>> curriedAdd = x -> y -> x + y;
+
+        Function<Integer, Integer> addThree = curriedAdd.apply(3);
+        int result = addThree.apply(10);
+        System.out.println("result : " + result);
+    }
+
+    public static Supplier<String> getStringSupplier() {
+        String hello = "Hello";
+
+        Supplier<String> supplier = () -> {
+            String world = "World";
+            return hello + " " + world;
+        };
+
+        return supplier;
+    }
+
+}
